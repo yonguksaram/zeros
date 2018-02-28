9b305b8630fc30f110fcb25f50526a91e2577c18
Stanislau Alimpau <alimberg@list.ru>
2018-02-28 16:26:13 +0300

    zeros/feature: adding code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let count = 0;
+  for(let i = 244140625; i>1; i=i/5) {
+    count+= Math.floor(number/i);
+  }
+  return count;
 }
